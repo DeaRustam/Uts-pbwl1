09978044f95cdb4069da3f2bf5db2deec547080f
DeaRustam <lestarideaa22@gmail.com>
2022-05-26 14:26:06 +0700

    Ini tugas uts pbwl

---
+++ views/kontak_tampil.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>PERPUSTAKAAN DEA ROKAN HILIR</strong></center></label>
+                            <p align="center">
+                              Jalan Makmur, Sintong, Tanah Putih<br>
+                              Kabupaten Rokan Hilir, Riau, Kode Pos : 28983
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 082285310259<br>
+                            Email : Lestarideaa22@gmail.com<br>
+                            Website : perpusdea.go.id<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
